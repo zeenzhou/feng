c900fe2e6623efc87803d6dcdf2748416fce4e02
zhouliang <zhouliang@zcmall.com>
2016-12-18 00:39:20 +0800

    add commit.

--- src/com/feng/hello/Feng.java
+++ src/com/feng/hello/Feng.java
@@ -3,5 +3,7 @@ package com.feng.hello;
 public class Feng {
 	public static void main(String[] args) {
 		System.out.println("Hello,feng,World.");
+		System.out.println("OK");
+		System.out.println("OK");
 	}
 }
